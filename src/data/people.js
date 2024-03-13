b756d01031f1891c3e4e0851a252e3db96792ba3
Riya Sabu <riyasabures@gmail.com>
2024-03-13 20:00:57 +0530

    added reps

--- src/data/people.js
+++ src/data/people.js
@@ -59,7 +59,7 @@ export const reps = [
         
     },
     {
-        img: "/people/reps/surya.jpg",
+        img: "/people/reps/surya.png",
         title: "Suryagayatri Thangalazhi",
         subtitle: "B.Tech, Electronics and Communication",
        
