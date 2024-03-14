d939d21e7f0964656968f67838998802c0e8b004
Riya Sabu <riyasabures@gmail.com>
2024-03-14 07:35:24 +0530

    added just pics

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,4 +1,161 @@
 export const gallery = [
+    export const gallery = [
+        {
+            src: "/gallery/archie-building.jpg",
+            width: 4,
+            height: 3,
+            desc: "Department of Architecture",
+        },
+        {
+            src: "/gallery/main-building.jpg",
+            width: 4,
+            height: 3,
+            desc: "Main building",
+        },
+        {
+            src: "/gallery/cetaa-hall.jpg",
+            width: 3,
+            height: 4,
+            desc: "CETAA Hall",
+        },
+        {
+            src: "/gallery/cgpu-building.jpg",
+            width: 1,
+            height: 1,
+            desc: "CGPU Building",
+        },
+        {
+            src: "/gallery/cgpu-hall.jpg",
+            width: 3,
+            height: 4,
+            desc: "CGPU Hall",
+        },
+        {
+            src: "/gallery/civil-building.jpg",
+            width: 3,
+            height: 4,
+            desc: "Department of Civil Engineering",
+        },
+        {
+            src: "/gallery/civil-canteen.jpg",
+            width: 3,
+            height: 4,
+            desc: "Civil Canteen",
+        },
+        {
+            src: "/gallery/computer-building.jpg",
+            width: 4,
+            height: 3,
+            desc: "Dept. of Computer Science & Engineering",
+        },
+        {
+            src: "/gallery/conference-hall.jpg",
+            width: 3,
+            height: 4,
+            desc: "Conference Hall",
+        },
+        {
+            src: "/gallery/dhwani-highlights.jpg",
+            width: 4,
+            height: 3,
+            desc: "Dhwani Highlights",
+        },
+        {
+            src: "/gallery/dj-hall.jpg",
+            width: 3,
+            height: 4,
+            desc: "Diamond Jubilee Hall",
+        },
+        {
+            src: "/gallery/dj-hall-interior.jpg",
+            width: 4,
+            height: 3,
+            desc: "Interior of Diamond Jubilee Hall",
+        },
+        {
+            src: "/gallery/electrical-building.jpg",
+            width: 4,
+            height: 3,
+            desc: "Department of Electrical and Electronics Engineering",
+        },
+        {
+            src: "/gallery/electronics-building.jpg",
+            width: 4,
+            height: 3,
+            desc: "Department of Electronics and Communication Engineering",
+        },
+        {
+            src: "/gallery/golden-plaque.jpg",
+            width: 1,
+            height: 1,
+            desc: "Golden Plaque",
+        },
+        {
+            src: "/gallery/graduation.jpg",
+            width: 3,
+            height: 4,
+            desc: "Graduation",
+        },
+        {
+            src: "/gallery/library-building.jpg",
+            width: 3,
+            height: 4,
+            desc: "Technical library",
+        },
+        {
+            src: "/gallery/library-interior.jpg",
+            width: 3,
+            height: 4,
+            desc: "Interior of library building",
+        },
+        {
+            src: "/gallery/mba-block.jpg",
+            width: 3,
+            height: 4,
+            desc: "MBA Block",
+        },
+        {
+            src: "/gallery/mca-building.jpg",
+            width: 3,
+            height: 4,
+            desc: "Department of Computer Applications",
+        },
+        {
+            src: "/gallery/mech-building.jpg",
+            width: 3,
+            height: 4,
+            desc: "Department of Mechanical Engineering",
+        },
+        {
+            src: "/gallery/pg-block.jpg",
+            width: 3,
+            height: 4,
+            desc: "PG Block of Mechanical Engineering",
+        },
+        {
+            src: "/gallery/playground.jpg",
+            width: 3,
+            height: 4,
+            desc: "College playground",
+        },
+        {
+            src: "/gallery/sargam-stage.jpg",
+            width: 3,
+            height: 4,
+            desc: "Sargam Stage",
+        },
+        {
+            src: "/gallery/aerial-view.jpg",
+            width: 1,
+            height: 1,
+            desc: "Aerial View of college",
+        },
+        {
+            src: "/gallery/cafeteria.jpg",
+            width: 4,
+            height: 3,
+            desc: "College Cafeteria",
+        },
     {
         src: "/gallery/archie-building.jpg",
         width: 4,
@@ -30,17 +187,20 @@ export const gallery = [
         desc: "CGPU Hall",
     },
     {
+        src: "/gallery/gd-room.jpg",
+        width: 3,
+        height: 4,
+        desc: "GD Room",
+
+    },
+    {
         src: "/gallery/civil-building.jpg",
         width: 3,
         height: 4,
         desc: "Department of Civil Engineering",
     },
-    {
-        src: "/gallery/civil-canteen.jpg",
-        width: 3,
-        height: 4,
-        desc: "Civil Canteen",
-    },
+    
+    
     {
         src: "/gallery/computer-building.jpg",
         width: 4,
@@ -60,14 +220,38 @@ export const gallery = [
         desc: "Dhwani Highlights",
     },
     {
-        src: "/gallery/dj-hall.jpg",
+        src: "/gallery/civil-canteen.jpg",
         width: 3,
+        height: 4,
+        desc: "Civil Canteen",
+    },
+    {
+        src: "/gallery/gazebo.jpg",
+        width: 3,
+        height: 4,
+        desc: "Gazebo",
+    },
+    {
+        src: "/gallery/physical-education.jpg",
+        width:4,
+        height:3,
+        desc: "Physical Education",
+    },
+    {
+        src: "/gallery/basket-court.jpg",
+        width:4,
+        height:3,
+        desc: "Basket-Ball Court",
+    },
+    {
+        src: "/gallery/dj-hall.jpg",
+        width: 4,
         height: 4,
         desc: "Diamond Jubilee Hall",
     },
     {
         src: "/gallery/dj-hall-interior.jpg",
-        width: 4,
+        width: 3,
         height: 3,
         desc: "Interior of Diamond Jubilee Hall",
     },
@@ -144,17 +328,24 @@ export const gallery = [
         desc: "Sargam Stage",
     },
     {
+        src: "/gallery/dwani-stage.jpg",
+        width: 4,
+        height: 3,
+        desc: "Dhwani Stage",
+    },
+    {
+        src: "/gallery/artswall.jpg",
+        width: 3,
+        height: 4,
+        desc: "Art Wall",
+    },
+    {
         src: "/gallery/aerial-view.jpg",
         width: 1,
         height: 1,
         desc: "Aerial View of college",
     },
-    {
-        src: "/gallery/cafeteria.jpg",
-        width: 4,
-        height: 3,
-        desc: "College Cafeteria",
-    },
+    
     // {
     //     src: "/gallery/gimg11.jpg",
     //     width: 1,
