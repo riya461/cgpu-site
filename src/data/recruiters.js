04c7625ae1b829ba0880b12bba4214d92fea7280
Srutav Tarun <srutavtarun@gmail.com>
2024-03-14 22:39:40 +0530

    added images, data in recruiters

--- src/data/recruiters.js
+++ src/data/recruiters.js
@@ -1,166 +1,687 @@
 export const recruiters = [
-    {
-        company: "Amazon",
-        url: "amazon.jobs",
-        logo: "/recruiters-logo/amazon.svg",
-    },
-    {
-        company: "Intuit",
-        url: "www.intuit.com",
-        logo: "/recruiters-logo/intuit.svg",
-    },
-    {
-        company: "Bosch",
-        url: "www.bosch.com",
-        logo: "/recruiters-logo/bosch.svg",
-    },
-    {
-        company: "GE",
-        url: "www.ge.com",
-        logo: "/recruiters-logo/ge.svg",
-    },
-    {
-        company: "IBM",
-        url: "www.ibm.com",
-        logo: "/recruiters-logo/ibm.svg",
-    },
-    {
-        company: "Infosys",
-        url: "www.infosys.com",
-        logo: "/recruiters-logo/infosys.svg",
-    },
-    {
-        company: "Siemens",
-        url: "www.siemens.com",
-        logo: "/recruiters-logo/siemens.svg",
-    },
-    {
-        company: "Amadeus",
-        url: "www.amadeus.com",
-        logo: "/recruiters-logo/amadeus.svg",
-    },
-    {
-        company: "Oracle",
-        url: "www.oracle.com",
-        logo: "/recruiters-logo/oracle.svg",
-    },
-    {
-        company: "Wipro",
-        url: "www.wipro.com",
-        logo: "/recruiters-logo/wipro.svg",
-    },
-    {
-        company: "Cognizant",
-        url: "www.cognizant.com",
-        logo: "/recruiters-logo/cognizant.svg",
-    },
-    {
-        company: "TCS",
-        url: "www.tcs.com",
-        logo: "/recruiters-logo/tcs.svg",
-    },
-    {
-        company: "Tismo",
-        url: "www.tismotech.com",
-        logo: "/recruiters-logo/tismo.svg",
-    },
-    {
-        company: "Zoho",
-        url: "www.zoho.com",
-        logo: "/recruiters-logo/zoho.svg",
-    },
-    {
-        company: "ABB",
-        url:"",
-        logo: "/recruiters-logo/abb.svg",
-    },
-    {
-        company: "Capgemini",
-        url: "",
-        logo: "/recruiters-logo/capgemini.svg",
-    },
-    {
-        company: "Saint-Gobain",
-        url: "",
-        logo: "/recruiters-logo/saint-gobain.svg",
-    },
-    {
-        company: "MRF",
-        url: "",
-        logo: "/recruiters-logo/mrf.svg",
-    },
-    {
-        company: "Accenture",
-        url: "",
-        logo: "/recruiters-logo/accenture.png",
-    },
-    {
-        company: "bridgei2i",
-        url: "",
-        logo: "/recruiters-logo/bridgei2i.png",
-    },
-    {
-        company: "Byju's",
-        url: "",
-        logo: "/recruiters-logo/byjus_logo.svg",
-    },
-    {
-        company: "Deloitte",
-        url: "",
-        logo: "/recruiters-logo/deloitte_logo.svg",
-    },
-    {
-        company: "Envestnet",
-        url: "",
-        logo: "/recruiters-logo/envestnet.svg",
-    },
-    {
-        company: "Experion",
-        url: "",
-        logo: "/recruiters-logo/experion_technologies_logo.svg",
-    },
-    {
-        company: "EY",
-        url: "",
-        logo: "/recruiters-logo/ey.svg",
-    },
-    {
-        company: "ITC",
-        url: "",
-        logo: "/recruiters-logo/itc.svg",
-    },
-    {
-        company: "L&T Tech",
-        url: "",
-        logo: "/recruiters-logo/l_t.svg",
-    },
-    {
-        company: "Mitsogo",
-        logo: "/recruiters-logo/mitsogo_inc_logo.svg",
-    },
-    {
-        company: "Providence",
-        url: "",
-        logo: "/recruiters-logo/providencehealthsystem_logo.svg",
-    },
-    {
-        company: "Quest Global",
-        url: "",
-        logo: "/recruiters-logo/quest_global_logo.svg",
-    },
-    {
-        company: "Tata Elxsi",
-        url: "",
-        logo: "/recruiters-logo/tata elxsi.svg",
-    },
-    {
-        company: "Texas Instruments",
-        url: "",
-        logo: "/recruiters-logo/texas_instruments_logo.svg",
-    },
-    {
-        company: "UST Global",
-        url: "",
-        logo: "/recruiters-logo/ust.svg",
-    },
+  {
+    company: "Amazon",
+    url: "amazon.jobs",
+    logo: "/recruiters-logo/amazon.svg",
+  },
+  {
+    company: "Intuit",
+    url: "www.intuit.com",
+    logo: "/recruiters-logo/intuit.svg",
+  },
+  {
+    company: "Bosch",
+    url: "www.bosch.com",
+    logo: "/recruiters-logo/bosch.svg",
+  },
+  {
+    company: "GE",
+    url: "www.ge.com",
+    logo: "/recruiters-logo/ge.svg",
+  },
+  {
+    company: "IBM",
+    url: "www.ibm.com",
+    logo: "/recruiters-logo/ibm.svg",
+  },
+  {
+    company: "Infosys",
+    url: "www.infosys.com",
+    logo: "/recruiters-logo/infosys.svg",
+  },
+  {
+    company: "Siemens",
+    url: "www.siemens.com",
+    logo: "/recruiters-logo/siemens.svg",
+  },
+  {
+    company: "Amadeus",
+    url: "www.amadeus.com",
+    logo: "/recruiters-logo/amadeus.svg",
+  },
+  {
+    company: "Oracle",
+    url: "www.oracle.com",
+    logo: "/recruiters-logo/oracle.svg",
+  },
+  {
+    company: "Wipro",
+    url: "www.wipro.com",
+    logo: "/recruiters-logo/wipro.svg",
+  },
+  {
+    company: "Cognizant",
+    url: "www.cognizant.com",
+    logo: "/recruiters-logo/cognizant.svg",
+  },
+  {
+    company: "TCS",
+    url: "www.tcs.com",
+    logo: "/recruiters-logo/tcs.svg",
+  },
+  {
+    company: "Tismo",
+    url: "www.tismotech.com",
+    logo: "/recruiters-logo/tismo.svg",
+  },
+  {
+    company: "Zoho",
+    url: "www.zoho.com",
+    logo: "/recruiters-logo/zoho.svg",
+  },
+  {
+    company: "ABB",
+    url: "",
+    logo: "/recruiters-logo/abb.svg",
+  },
+  {
+    company: "Capgemini",
+    url: "",
+    logo: "/recruiters-logo/capgemini.svg",
+  },
+  {
+    company: "Saint-Gobain",
+    url: "",
+    logo: "/recruiters-logo/saint-gobain.svg",
+  },
+  {
+    company: "MRF",
+    url: "",
+    logo: "/recruiters-logo/mrf.svg",
+  },
+  {
+    company: "Accenture",
+    url: "",
+    logo: "/recruiters-logo/accenture.png",
+  },
+  {
+    company: "bridgei2i",
+    url: "",
+    logo: "/recruiters-logo/bridgei2i.png",
+  },
+  {
+    company: "Byju's",
+    url: "",
+    logo: "/recruiters-logo/byjus_logo.svg",
+  },
+  {
+    company: "Deloitte",
+    url: "",
+    logo: "/recruiters-logo/deloitte_logo.svg",
+  },
+  {
+    company: "Envestnet",
+    url: "",
+    logo: "/recruiters-logo/envestnet.svg",
+  },
+  {
+    company: "Experion",
+    url: "",
+    logo: "/recruiters-logo/experion_technologies_logo.svg",
+  },
+  {
+    company: "EY",
+    url: "",
+    logo: "/recruiters-logo/ey.svg",
+  },
+  {
+    company: "ITC",
+    url: "",
+    logo: "/recruiters-logo/itc.svg",
+  },
+  {
+    company: "L&T Tech",
+    url: "",
+    logo: "/recruiters-logo/l_t.svg",
+  },
+  {
+    company: "Mitsogo",
+    logo: "/recruiters-logo/mitsogo_inc_logo.svg",
+  },
+  {
+    company: "Providence",
+    url: "",
+    logo: "/recruiters-logo/providencehealthsystem_logo.svg",
+  },
+  {
+    company: "Quest Global",
+    url: "",
+    logo: "/recruiters-logo/quest_global_logo.svg",
+  },
+  {
+    company: "Tata Elxsi",
+    url: "",
+    logo: "/recruiters-logo/tata elxsi.svg",
+  },
+  {
+    company: "Texas Instruments",
+    url: "",
+    logo: "/recruiters-logo/texas_instruments_logo.svg",
+  },
+  {
+    company: "UST Global",
+    url: "",
+    logo: "/recruiters-logo/ust.svg",
+  },
+
+  {
+    company: "Aakash Byjus",
+    url: "example.com/aakash-byjus",
+    logo: "/recruiters-logo/Aakash Byjus.png",
+  },
+  {
+    company: "Aarbee",
+    url: "example.com/aarbee",
+    logo: "/recruiters-logo/Aarbee.png",
+  },
+  {
+    company: "ABB",
+    url: "example.com/abb",
+    logo: "/recruiters-logo/ABB.png",
+  },
+  {
+    company: "Academor",
+    url: "example.com/academor",
+    logo: "/recruiters-logo/Academor.png",
+  },
+  {
+    company: "Accenture",
+    url: "example.com/accenture",
+    logo: "/recruiters-logo/Accenture.png",
+  },
+  {
+    company: "Acsia",
+    url: "example.com/acsia",
+    logo: "/recruiters-logo/Acsia.png",
+  },
+  {
+    company: "Adobe",
+    url: "example.com/adobe",
+    logo: "/recruiters-logo/Adobe.png",
+  },
+  {
+    company: "AG&P",
+    url: "example.com/agp",
+    logo: "/recruiters-logo/AG&P.png",
+  },
+  {
+    company: "Alamy",
+    url: "example.com/alamy",
+    logo: "/recruiters-logo/Alamy.png",
+  },
+  {
+    company: "Alight",
+    url: "example.com/alight",
+    logo: "/recruiters-logo/alight.png",
+  },
+  {
+    company: "Amadeus",
+    url: "example.com/amadeus",
+    logo: "/recruiters-logo/Amadeus.png",
+  },
+  {
+    company: "Asian Paints",
+    url: "example.com/asian-paints",
+    logo: "/recruiters-logo/asian paints.png",
+  },
+  {
+    company: "AurionPro",
+    url: "example.com/aurionpro",
+    logo: "/recruiters-logo/AurionPro.png",
+  },
+  {
+    company: "Bennett Coleman",
+    url: "example.com/bennett-coleman",
+    logo: "/recruiters-logo/Bennett Coleman.png",
+  },
+  {
+    company: "Bluebinaries",
+    url: "example.com/bluebinaries",
+    logo: "/recruiters-logo/Bluebinaries.png",
+  },
+  {
+    company: "Bosch",
+    url: "example.com/bosch",
+    logo: "/recruiters-logo/Bosch.png",
+  },
+  {
+    company: "Capgemini",
+    url: "example.com/capgemini",
+    logo: "/recruiters-logo/Capgemini.png",
+  },
+  {
+    company: "CGI",
+    url: "example.com/cgi",
+    logo: "/recruiters-logo/CGI.png",
+  },
+  {
+    company: "CMS",
+    url: "example.com/cms",
+    logo: "/recruiters-logo/CMS.jpg",
+  },
+  {
+    company: "Codeyoung",
+    url: "example.com/codeyoung",
+    logo: "/recruiters-logo/Codeyoung.png",
+  },
+  {
+    company: "CTS",
+    url: "example.com/cts",
+    logo: "/recruiters-logo/CTS.png",
+  },
+  {
+    company: "CynLr",
+    url: "example.com/cynlr",
+    logo: "/recruiters-logo/CynLr.png",
+  },
+  {
+    company: "Daimler",
+    url: "example.com/daimler",
+    logo: "/recruiters-logo/Daimler.jpg",
+  },
+  {
+    company: "Deloitte",
+    url: "example.com/deloitte",
+    logo: "/recruiters-logo/Deloitte.png",
+  },
+  {
+    company: "Elegant Marine",
+    url: "example.com/elegant-marine",
+    logo: "/recruiters-logo/Elegant Marine.jpg",
+  },
+  {
+    company: "Elucidata",
+    url: "example.com/elucidata",
+    logo: "/recruiters-logo/Elucidata.svg",
+  },
+  {
+    company: "Envestnet Inc",
+    url: "example.com/envestnet-inc",
+    logo: "/recruiters-logo/envestnet__inc__logo.jpg",
+  },
+  {
+    company: "ESAF",
+    url: "example.com/esaf",
+    logo: "/recruiters-logo/ESAF.png",
+  },
+  {
+    company: "Experion",
+    url: "example.com/experion",
+    logo: "/recruiters-logo/Experion.png",
+  },
+  {
+    company: "EY",
+    url: "example.com/ey",
+    logo: "/recruiters-logo/EY.png",
+  },
+  {
+    company: "Federal Bank",
+    url: "example.com/federal-bank",
+    logo: "/recruiters-logo/Federal Bank.png",
+  },
+  {
+    company: "Ford",
+    url: "example.com/ford",
+    logo: "/recruiters-logo/Ford.jpeg",
+  },
+  {
+    company: "Ganit",
+    url: "example.com/ganit",
+    logo: "/recruiters-logo/Ganit.png",
+  },
+  {
+    company: "Gemini",
+    url: "example.com/gemini",
+    logo: "/recruiters-logo/Gemini.svg",
+  },
+  {
+    company: "Guidehouse",
+    url: "example.com/guidehouse",
+    logo: "/recruiters-logo/Guidehouse_logo.png",
+  },
+  {
+    company: "Havells",
+    url: "example.com/havells",
+    logo: "/recruiters-logo/Havells.png",
+  },
+  {
+    company: "HCL",
+    url: "example.com/hcl",
+    logo: "/recruiters-logo/HCL.png",
+  },
+  {
+    company: "IBM",
+    url: "example.com/ibm",
+    logo: "/recruiters-logo/IBM.svg",
+  },
+  {
+    company: "IBS",
+    url: "example.com/ibs",
+    logo: "/recruiters-logo/IBS.png",
+  },
+  {
+    company: "ICICI",
+    url: "example.com/icici",
+    logo: "/recruiters-logo/icici.png",
+  },
+  {
+    company: "Ignitarium",
+    url: "example.com/ignitarium",
+    logo: "/recruiters-logo/Ignitarium.png",
+  },
+  {
+    company: "Incture",
+    url: "example.com/incture",
+    logo: "/recruiters-logo/Incture.png",
+  },
+  {
+    company: "Inflection",
+    url: "example.com/inflection",
+    logo: "/recruiters-logo/Inflection.png",
+  },
+  {
+    company: "Intellipaat",
+    url: "example.com/intellipaat",
+    logo: "/recruiters-logo/Intellipaat.jpg",
+  },
+  {
+    company: "ITC",
+    url: "example.com/itc",
+    logo: "/recruiters-logo/ITC.png",
+  },
+  {
+    company: "ITD",
+    url: "example.com/itd",
+    logo: "/recruiters-logo/ITD.jpg",
+  },
+  {
+    company: "JSW",
+    url: "example.com/jsw",
+    logo: "/recruiters-logo/JSW.png",
+  },
+  {
+    company: "Kantar",
+    url: "example.com/kantar",
+    logo: "/recruiters-logo/Kantar.png",
+  },
+  {
+    company: "Keyvalue",
+    url: "example.com/keyvalue",
+    logo: "/recruiters-logo/Keyvalue.svg",
+  },
+  {
+    company: "KPMG",
+    url: "example.com/kpmg",
+    logo: "/recruiters-logo/KPMG.svg",
+  },
+  {
+    company: "L&T",
+    url: "example.com/landt",
+    logo: "/recruiters-logo/L&T.png",
+  },
+  {
+    company: "Logicamatrix",
+    url: "example.com/logicamatrix",
+    logo: "/recruiters-logo/Logicamatrix.jpg",
+  },
+  {
+    company: "LTIMindtree",
+    url: "example.com/ltimindtree",
+    logo: "/recruiters-logo/LTIMindtree.jpg",
+  },
+  {
+    company: "LTTS",
+    url: "example.com/ltts",
+    logo: "/recruiters-logo/LTTS.jpg",
+  },
+  {
+    company: "Mewurk",
+    url: "example.com/mewurk",
+    logo: "/recruiters-logo/Mewurk.jpg",
+  },
+  {
+    company: "Microchip",
+    url: "example.com/microchip",
+    logo: "/recruiters-logo/Microchip.png",
+  },
+  {
+    company: "Mindteck",
+    url: "example.com/mindteck",
+    logo: "/recruiters-logo/Mindteck.jpg",
+  },
+  {
+    company: "MIPS",
+    url: "example.com/mips",
+    logo: "/recruiters-logo/MIPS.png",
+  },
+  {
+    company: "MRF",
+    url: "example.com/mrf",
+    logo: "/recruiters-logo/MRF.png",
+  },
+  {
+    company: "MyCaptain",
+    url: "example.com/mycaptain",
+    logo: "/recruiters-logo/MyCaptain.svg",
+  },
+  {
+    company: "NeoITO",
+    url: "example.com/neoit",
+    logo: "/recruiters-logo/NeoITO.png",
+  },
+  {
+    company: "NEST Digital",
+    url: "example.com/nest-digital",
+    logo: "/recruiters-logo/NEST Digital.png",
+  },
+  {
+    company: "Nissan",
+    url: "example.com/nissan",
+    logo: "/recruiters-logo/Nissan.jpg",
+  },
+  {
+    company: "Nokia",
+    url: "example.com/nokia",
+    logo: "/recruiters-logo/Nokia.jpg",
+  },
+  {
+    company: "NTT Data",
+    url: "example.com/ntt-data",
+    logo: "/recruiters-logo/NTT Data.jpg",
+  },
+  {
+    company: "o9 Solutions",
+    url: "example.com/o9-solutions",
+    logo: "/recruiters-logo/o9-Solutions.jpg",
+  },
+  {
+    company: "Oracle",
+    url: "example.com/oracle",
+    logo: "/recruiters-logo/oracle.png",
+  },
+  {
+    company: "Pie Infocomm",
+    url: "example.com/pie-infocomm",
+    logo: "/recruiters-logo/pie infocomm.jpg",
+  },
+  {
+    company: "Prevalent",
+    url: "example.com/prevalent",
+    logo: "/recruiters-logo/prevalent.png",
+  },
+  {
+    company: "Providence",
+    url: "example.com/providence",
+    logo: "/recruiters-logo/providence.jpg",
+  },
+  {
+    company: "PwC",
+    url: "example.com/pwc",
+    logo: "/recruiters-logo/pwc.png",
+  },
+  {
+    company: "Qburst",
+    url: "example.com/qburst",
+    logo: "/recruiters-logo/qburst.png",
+  },
+  {
+    company: "Qualitest",
+    url: "example.com/qualitest",
+    logo: "/recruiters-logo/qualitest.jpg",
+  },
+  {
+    company: "Quantiphi",
+    url: "example.com/quantiphi",
+    logo: "/recruiters-logo/quantiphi.png",
+  },
+  {
+    company: "Questglobal",
+    url: "example.com/questglobal",
+    logo: "/recruiters-logo/questglobal.jpg",
+  },
+  {
+    company: "Ramco Cements",
+    url: "example.com/ramco-cements",
+    logo: "/recruiters-logo/ramco cements.jpg",
+  },
+  {
+    company: "Ramco",
+    url: "example.com/ramco",
+    logo: "/recruiters-logo/ramco.avif",
+  },
+  {
+    company: "Reliance BP",
+    url: "example.com/reliance-bp",
+    logo: "/recruiters-logo/reliance bp.png",
+  },
+  {
+    company: "Royal Anfield",
+    url: "example.com/royal-anfield",
+    logo: "/recruiters-logo/royal anfield.png",
+  },
+  {
+    company: "SADA",
+    url: "example.com/sada",
+    logo: "/recruiters-logo/sada.jpg",
+  },
+  {
+    company: "Saint Gobain",
+    url: "example.com/saint-gobain",
+    logo: "/recruiters-logo/saint gobain.png",
+  },
+  {
+    company: "Schneider Electric",
+    url: "example.com/schneider-electric",
+    logo: "/recruiters-logo/schneider_electric_logo.png",
+  },
+  {
+    company: "Sentient Scripts",
+    url: "example.com/sentient-scripts",
+    logo: "/recruiters-logo/sentient scripts.png",
+  },
+  {
+    company: "SFO",
+    url: "example.com/sfo",
+    logo: "/recruiters-logo/sfo.jpg",
+  },
+  {
+    company: "Siemens",
+    url: "example.com/siemens",
+    logo: "/recruiters-logo/siemens.png",
+  },
+  {
+    company: "Smallday",
+    url: "example.com/smallday",
+    logo: "/recruiters-logo/smallday.jpg",
+  },
+  {
+    company: "Sobha Ltd",
+    url: "example.com/sobha-ltd",
+    logo: "/recruiters-logo/Sobha_Ltd_Logo.jpg",
+  },
+  {
+    company: "South Indian Bank",
+    url: "example.com/south-indian-bank",
+    logo: "/recruiters-logo/south indian bank.png",
+  },
+  {
+    company: "SP Global",
+    url: "example.com/sp-global",
+    logo: "/recruiters-logo/sp global.png",
+  },
+  {
+    company: "Tally",
+    url: "example.com/tally",
+    logo: "/recruiters-logo/Tally_-_Logo.png",
+  },
+  {
+    company: "TATAELXSI",
+    url: "example.com/tataelxsi",
+    logo: "/recruiters-logo/TATAELXSI.webp",
+  },
+  {
+    company: "TCS",
+    url: "example.com/tcs",
+    logo: "/recruiters-logo/tcs.png",
+  },
+  {
+    company: "Teachnook",
+    url: "example.com/teachnook",
+    logo: "/recruiters-logo/teachnook.png",
+  },
+  {
+    company: "Techvantage",
+    url: "example.com/techvantage",
+    logo: "/recruiters-logo/techvantage.jpg",
+  },
+  {
+    company: "TI",
+    url: "example.com/ti",
+    logo: "/recruiters-logo/ti.png",
+  },
+  {
+    company: "Tismo",
+    url: "example.com/tismo",
+    logo: "/recruiters-logo/tismo.jpg",
+  },
+  {
+    company: "Tredence",
+    url: "example.com/tredence",
+    logo: "/recruiters-logo/Tredence_logo.png",
+  },
+  {
+    company: "Varroc",
+    url: "example.com/varroc",
+    logo: "/recruiters-logo/varroc.png",
+  },
+  {
+    company: "Verteil",
+    url: "example.com/verteil",
+    logo: "/recruiters-logo/verteil.jpg",
+  },
+  {
+    company: "Visteon",
+    url: "example.com/visteon",
+    logo: "/recruiters-logo/visteon.png",
+  },
+  {
+    company: "Walkaroo",
+    url: "example.com/walkaroo",
+    logo: "/recruiters-logo/walkaroo.avif",
+  },
+  {
+    company: "Western Digital",
+    url: "example.com/western-digital",
+    logo: "/recruiters-logo/Western-Digital-logo.png",
+  },
+  {
+    company: "WSP",
+    url: "example.com/wsp",
+    logo: "/recruiters-logo/wsp.png",
+  },
+  {
+    company: "ZF Group",
+    url: "example.com/zf-group",
+    logo: "/recruiters-logo/zf group.png",
+  },
 ];
